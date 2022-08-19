622a05f6751f982bd604376487287bfe453cb1dc
Evan Lynch <eclyn053@learner.dvusd.org>
2022-08-19 11:40:50 -0700

    thursday update
    yes

---
+++ VB/SnackDex/SnackDex/Form1.Designer.vb
@@ -0,0 +1,340 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.OreoButton = New System.Windows.Forms.Button()
+        Me.displayPictureBox = New System.Windows.Forms.PictureBox()
+        Me.OreoPictureBox = New System.Windows.Forms.PictureBox()
+        Me.NameLabrel = New System.Windows.Forms.Label()
+        Me.packagingLabel = New System.Windows.Forms.Label()
+        Me.yummyLabel = New System.Windows.Forms.Label()
+        Me.SnackLabel = New System.Windows.Forms.Label()
+        Me.packagecolorLabel = New System.Windows.Forms.Label()
+        Me.deliciousLabel = New System.Windows.Forms.Label()
+        Me.CheezItzButton = New System.Windows.Forms.Button()
+        Me.CheezItzPictureBox = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.wouldbuy = New System.Windows.Forms.Label()
+        Me.LaysButton = New System.Windows.Forms.Button()
+        Me.littlebitesButton = New System.Windows.Forms.Button()
+        Me.tostitosButton = New System.Windows.Forms.Button()
+        Me.LaysPictureBox = New System.Windows.Forms.PictureBox()
+        Me.LittleBitesPictureBox = New System.Windows.Forms.PictureBox()
+        Me.TostitosPictureBox = New System.Windows.Forms.PictureBox()
+        Me.reviewishLabel = New System.Windows.Forms.Label()
+        Me.reviewLabel = New System.Windows.Forms.Label()
+        Me.thoughtsButton = New System.Windows.Forms.Button()
+        Me.thoughtsLabel = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.OreoPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CheezItzPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LaysPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LittleBitesPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TostitosPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'OreoButton
+        '
+        Me.OreoButton.Location = New System.Drawing.Point(221, 603)
+        Me.OreoButton.Name = "OreoButton"
+        Me.OreoButton.Size = New System.Drawing.Size(75, 23)
+        Me.OreoButton.TabIndex = 0
+        Me.OreoButton.Text = "Oreo"
+        Me.OreoButton.UseVisualStyleBackColor = True
+        '
+        'displayPictureBox
+        '
+        Me.displayPictureBox.Location = New System.Drawing.Point(55, 178)
+        Me.displayPictureBox.Name = "displayPictureBox"
+        Me.displayPictureBox.Size = New System.Drawing.Size(570, 338)
+        Me.displayPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.displayPictureBox.TabIndex = 1
+        Me.displayPictureBox.TabStop = False
+        '
+        'OreoPictureBox
+        '
+        Me.OreoPictureBox.Image = CType(resources.GetObject("OreoPictureBox.Image"), System.Drawing.Image)
+        Me.OreoPictureBox.Location = New System.Drawing.Point(221, 642)
+        Me.OreoPictureBox.Name = "OreoPictureBox"
+        Me.OreoPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.OreoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.OreoPictureBox.TabIndex = 2
+        Me.OreoPictureBox.TabStop = False
+        '
+        'NameLabrel
+        '
+        Me.NameLabrel.AutoSize = True
+        Me.NameLabrel.Location = New System.Drawing.Point(645, 278)
+        Me.NameLabrel.Name = "NameLabrel"
+        Me.NameLabrel.Size = New System.Drawing.Size(47, 16)
+        Me.NameLabrel.TabIndex = 3
+        Me.NameLabrel.Text = "Name:"
+        '
+        'packagingLabel
+        '
+        Me.packagingLabel.AutoSize = True
+        Me.packagingLabel.Location = New System.Drawing.Point(645, 328)
+        Me.packagingLabel.Name = "packagingLabel"
+        Me.packagingLabel.Size = New System.Drawing.Size(100, 16)
+        Me.packagingLabel.TabIndex = 4
+        Me.packagingLabel.Text = "Package Color:"
+        '
+        'yummyLabel
+        '
+        Me.yummyLabel.AutoSize = True
+        Me.yummyLabel.Location = New System.Drawing.Point(645, 380)
+        Me.yummyLabel.Name = "yummyLabel"
+        Me.yummyLabel.Size = New System.Drawing.Size(95, 16)
+        Me.yummyLabel.TabIndex = 5
+        Me.yummyLabel.Text = "Deliciousness:"
+        '
+        'SnackLabel
+        '
+        Me.SnackLabel.AutoSize = True
+        Me.SnackLabel.Location = New System.Drawing.Point(746, 278)
+        Me.SnackLabel.Name = "SnackLabel"
+        Me.SnackLabel.Size = New System.Drawing.Size(0, 16)
+        Me.SnackLabel.TabIndex = 6
+        '
+        'packagecolorLabel
+        '
+        Me.packagecolorLabel.AutoSize = True
+        Me.packagecolorLabel.Location = New System.Drawing.Point(746, 328)
+        Me.packagecolorLabel.Name = "packagecolorLabel"
+        Me.packagecolorLabel.Size = New System.Drawing.Size(0, 16)
+        Me.packagecolorLabel.TabIndex = 7
+        '
+        'deliciousLabel
+        '
+        Me.deliciousLabel.AutoSize = True
+        Me.deliciousLabel.Location = New System.Drawing.Point(746, 380)
+        Me.deliciousLabel.Name = "deliciousLabel"
+        Me.deliciousLabel.Size = New System.Drawing.Size(0, 16)
+        Me.deliciousLabel.TabIndex = 8
+        '
+        'CheezItzButton
+        '
+        Me.CheezItzButton.Location = New System.Drawing.Point(349, 603)
+        Me.CheezItzButton.Name = "CheezItzButton"
+        Me.CheezItzButton.Size = New System.Drawing.Size(75, 23)
+        Me.CheezItzButton.TabIndex = 9
+        Me.CheezItzButton.Text = "Cheez-Itz"
+        Me.CheezItzButton.UseVisualStyleBackColor = True
+        '
+        'CheezItzPictureBox
+        '
+        Me.CheezItzPictureBox.Image = CType(resources.GetObject("CheezItzPictureBox.Image"), System.Drawing.Image)
+        Me.CheezItzPictureBox.Location = New System.Drawing.Point(349, 642)
+        Me.CheezItzPictureBox.Name = "CheezItzPictureBox"
+        Me.CheezItzPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.CheezItzPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.CheezItzPictureBox.TabIndex = 10
+        Me.CheezItzPictureBox.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(645, 434)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(140, 16)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Would Regularly Buy?"
+        '
+        'wouldbuy
+        '
+        Me.wouldbuy.AutoSize = True
+        Me.wouldbuy.Location = New System.Drawing.Point(791, 434)
+        Me.wouldbuy.Name = "wouldbuy"
+        Me.wouldbuy.Size = New System.Drawing.Size(0, 16)
+        Me.wouldbuy.TabIndex = 12
+        '
+        'LaysButton
+        '
+        Me.LaysButton.Location = New System.Drawing.Point(474, 603)
+        Me.LaysButton.Name = "LaysButton"
+        Me.LaysButton.Size = New System.Drawing.Size(75, 23)
+        Me.LaysButton.TabIndex = 13
+        Me.LaysButton.Text = "Lays Barbacue"
+        Me.LaysButton.UseVisualStyleBackColor = True
+        '
+        'littlebitesButton
+        '
+        Me.littlebitesButton.Location = New System.Drawing.Point(601, 603)
+        Me.littlebitesButton.Name = "littlebitesButton"
+        Me.littlebitesButton.Size = New System.Drawing.Size(91, 23)
+        Me.littlebitesButton.TabIndex = 14
+        Me.littlebitesButton.Text = "Little Bites"
+        Me.littlebitesButton.UseVisualStyleBackColor = True
+        '
+        'tostitosButton
+        '
+        Me.tostitosButton.Location = New System.Drawing.Point(727, 603)
+        Me.tostitosButton.Name = "tostitosButton"
+        Me.tostitosButton.Size = New System.Drawing.Size(75, 23)
+        Me.tostitosButton.TabIndex = 15
+        Me.tostitosButton.Text = "Tostitos"
+        Me.tostitosButton.UseVisualStyleBackColor = True
+        '
+        'LaysPictureBox
+        '
+        Me.LaysPictureBox.Image = CType(resources.GetObject("LaysPictureBox.Image"), System.Drawing.Image)
+        Me.LaysPictureBox.Location = New System.Drawing.Point(474, 642)
+        Me.LaysPictureBox.Name = "LaysPictureBox"
+        Me.LaysPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.LaysPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.LaysPictureBox.TabIndex = 16
+        Me.LaysPictureBox.TabStop = False
+        '
+        'LittleBitesPictureBox
+        '
+        Me.LittleBitesPictureBox.Image = CType(resources.GetObject("LittleBitesPictureBox.Image"), System.Drawing.Image)
+        Me.LittleBitesPictureBox.Location = New System.Drawing.Point(601, 642)
+        Me.LittleBitesPictureBox.Name = "LittleBitesPictureBox"
+        Me.LittleBitesPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.LittleBitesPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.LittleBitesPictureBox.TabIndex = 17
+        Me.LittleBitesPictureBox.TabStop = False
+        '
+        'TostitosPictureBox
+        '
+        Me.TostitosPictureBox.Image = CType(resources.GetObject("TostitosPictureBox.Image"), System.Drawing.Image)
+        Me.TostitosPictureBox.Location = New System.Drawing.Point(727, 642)
+        Me.TostitosPictureBox.Name = "TostitosPictureBox"
+        Me.TostitosPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.TostitosPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.TostitosPictureBox.TabIndex = 18
+        Me.TostitosPictureBox.TabStop = False
+        '
+        'reviewishLabel
+        '
+        Me.reviewishLabel.AutoSize = True
+        Me.reviewishLabel.Location = New System.Drawing.Point(645, 484)
+        Me.reviewishLabel.Name = "reviewishLabel"
+        Me.reviewishLabel.Size = New System.Drawing.Size(55, 16)
+        Me.reviewishLabel.TabIndex = 19
+        Me.reviewishLabel.Text = "Review:"
+        '
+        'reviewLabel
+        '
+        Me.reviewLabel.AutoSize = True
+        Me.reviewLabel.Location = New System.Drawing.Point(700, 484)
+        Me.reviewLabel.Name = "reviewLabel"
+        Me.reviewLabel.Size = New System.Drawing.Size(0, 16)
+        Me.reviewLabel.TabIndex = 20
+        '
+        'thoughtsButton
+        '
+        Me.thoughtsButton.Location = New System.Drawing.Point(1070, 278)
+        Me.thoughtsButton.Name = "thoughtsButton"
+        Me.thoughtsButton.Size = New System.Drawing.Size(97, 40)
+        Me.thoughtsButton.TabIndex = 21
+        Me.thoughtsButton.Text = "Thoughts?"
+        Me.thoughtsButton.UseVisualStyleBackColor = True
+        '
+        'thoughtsLabel
+        '
+        Me.thoughtsLabel.AutoSize = True
+        Me.thoughtsLabel.Location = New System.Drawing.Point(1053, 328)
+        Me.thoughtsLabel.Name = "thoughtsLabel"
+        Me.thoughtsLabel.Size = New System.Drawing.Size(0, 16)
+        Me.thoughtsLabel.TabIndex = 22
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 4.6!)
+        Me.Button1.Location = New System.Drawing.Point(1173, 289)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 23)
+        Me.Button1.TabIndex = 23
+        Me.Button1.Text = "Close Thoughts"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1584, 708)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.thoughtsLabel)
+        Me.Controls.Add(Me.thoughtsButton)
+        Me.Controls.Add(Me.reviewLabel)
+        Me.Controls.Add(Me.reviewishLabel)
+        Me.Controls.Add(Me.TostitosPictureBox)
+        Me.Controls.Add(Me.LittleBitesPictureBox)
+        Me.Controls.Add(Me.LaysPictureBox)
+        Me.Controls.Add(Me.tostitosButton)
+        Me.Controls.Add(Me.littlebitesButton)
+        Me.Controls.Add(Me.LaysButton)
+        Me.Controls.Add(Me.wouldbuy)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheezItzPictureBox)
+        Me.Controls.Add(Me.CheezItzButton)
+        Me.Controls.Add(Me.deliciousLabel)
+        Me.Controls.Add(Me.packagecolorLabel)
+        Me.Controls.Add(Me.SnackLabel)
+        Me.Controls.Add(Me.yummyLabel)
+        Me.Controls.Add(Me.packagingLabel)
+        Me.Controls.Add(Me.NameLabrel)
+        Me.Controls.Add(Me.OreoPictureBox)
+        Me.Controls.Add(Me.displayPictureBox)
+        Me.Controls.Add(Me.OreoButton)
+        Me.Name = "Form1"
+        Me.Text = "SnackDex"
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.OreoPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CheezItzPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LaysPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LittleBitesPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TostitosPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OreoButton As Button
+    Friend WithEvents displayPictureBox As PictureBox
+    Friend WithEvents OreoPictureBox As PictureBox
+    Friend WithEvents NameLabrel As Label
+    Friend WithEvents packagingLabel As Label
+    Friend WithEvents yummyLabel As Label
+    Friend WithEvents SnackLabel As Label
+    Friend WithEvents packagecolorLabel As Label
+    Friend WithEvents deliciousLabel As Label
+    Friend WithEvents CheezItzButton As Button
+    Friend WithEvents CheezItzPictureBox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents wouldbuy As Label
+    Friend WithEvents LaysButton As Button
+    Friend WithEvents littlebitesButton As Button
+    Friend WithEvents tostitosButton As Button
+    Friend WithEvents LaysPictureBox As PictureBox
+    Friend WithEvents LittleBitesPictureBox As PictureBox
+    Friend WithEvents TostitosPictureBox As PictureBox
+    Friend WithEvents reviewishLabel As Label
+    Friend WithEvents reviewLabel As Label
+    Friend WithEvents thoughtsButton As Button
+    Friend WithEvents thoughtsLabel As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ VB/SnackDex/SnackDex/Form1.vb
@@ -0,0 +1,137 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles OreoButton.Click
+        displayPictureBox.Image = OreoPictureBox.Image
+        SnackLabel.Text = "Oreos"
+        packagecolorLabel.Text = "Blue"
+        deliciousLabel.Text = "10\10"
+        wouldbuy.Text = "Yes"
+        reviewLabel.Text = "Utterly Awesome, would eat as much as possible if I ever saw a box and if
+I was allowed I would eat as much as possible. There's a reason this is 10/10"
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles CheezItzButton.Click
+        displayPictureBox.Image = CheezItzPictureBox.Image
+        SnackLabel.Text = "Cheez-Itz"
+        packagecolorLabel.Text = "Red"
+        deliciousLabel.Text = "8/10"
+        wouldbuy.Text = "Yes"
+        reviewLabel.Text = "Amazing, would eat any day. I love this snack a lot. It is very good if
+you are starving. Except if you eat a lot. It feels terrible if you eat way to much due to the salt. And if you are
+no where near water, good luck."
+    End Sub
+
+    Private Sub LaysButton_Click(sender As Object, e As EventArgs) Handles LaysButton.Click
+        displayPictureBox.Image = LaysPictureBox.Image
+        SnackLabel.Text = "Lays"
+        packagecolorLabel.Text = "Yellow"
+        deliciousLabel.Text = "5/10 There are other options."
+        wouldbuy.Text = "No"
+        reviewLabel.Text = "This chip is alright. i would eat but like, I would like to have any
+other option. Even though it's the original Lays. This chip is just alright."
+    End Sub
+
+    Private Sub littlebitesButton_Click(sender As Object, e As EventArgs) Handles littlebitesButton.Click
+        displayPictureBox.Image = LittleBitesPictureBox.Image
+        SnackLabel.Text = "Little Bites"
+        packagecolorLabel.Text = "(Mainly) White"
+        deliciousLabel.Text = "9/10"
+        wouldbuy.Text = "Yes"
+        reviewLabel.Text = "Little Bites are these muffin things. I love all of the flavors.
+They are all amazing. Eat any time I saw one. The only downside is that there are only 4 muffins per bag."
+    End Sub
+
+    Private Sub tostitosButton_Click(sender As Object, e As EventArgs) Handles tostitosButton.Click
+        displayPictureBox.Image = TostitosPictureBox.Image
+        SnackLabel.Text = "Tostitos"
+        packagecolorLabel.Text = "Light Blue"
+        deliciousLabel.Text = "7/10"
+        wouldbuy.Text = "No"
+        reviewLabel.Text = "Tostitos are awesome, i love them with the store bought salsa. It has nice slaty sour
+feeling on your tongue. Sadly, if there are no salsa nearby, don't even think of eating these."
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    
+    Private Sub thoughtsButton_Click(sender As Object, e As EventArgs) Handles thoughtsButton.Click
+        thoughtsLabel.Text = "The ranking system from 1 to 10 now is pretty flawed. People now think that 7/10
+is an average ranking. And a 5/10 means that it is pretty bad. This leaves the fact that there are only
+3 ranking spots for good things. And 6 spots for bad things. Which is honestly pretty unfair for the good things.
+I think this has to do with today's grading system. Which a 70%, or a 7/10 is average. Which is pretty fair due
+to the fact that humans are number 1 on the big brain scale which was put together by humans. Anyway, I think
+people, should adjust the way we see this scale so that the middle, can be seen as the middle of the scale"
+    End Sub
+
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles Button1.Click
+        thoughtsLabel.Text = ""
+    End Sub
+
+    Private Sub displayPictureBox_Click(sender As Object, e As EventArgs) Handles displayPictureBox.Click
+
+    End Sub
+
+    Private Sub thoughtsLabel_Click(sender As Object, e As EventArgs) Handles thoughtsLabel.Click
+
+    End Sub
+
+    Private Sub reviewLabel_Click(sender As Object, e As EventArgs) Handles reviewLabel.Click
+
+    End Sub
+
+    Private Sub reviewishLabel_Click(sender As Object, e As EventArgs) Handles reviewishLabel.Click
+
+    End Sub
+
+    Private Sub LittleBitesPictureBox_Click(sender As Object, e As EventArgs) Handles LittleBitesPictureBox.Click
+
+    End Sub
+
+    Private Sub LaysPictureBox_Click(sender As Object, e As EventArgs) Handles LaysPictureBox.Click
+
+    End Sub
+
+    Private Sub wouldbuy_Click(sender As Object, e As EventArgs) Handles wouldbuy.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub CheezItzPictureBox_Click(sender As Object, e As EventArgs) Handles CheezItzPictureBox.Click
+
+    End Sub
+
+    Private Sub deliciousLabel_Click(sender As Object, e As EventArgs) Handles deliciousLabel.Click
+
+    End Sub
+
+    Private Sub packagecolorLabel_Click(sender As Object, e As EventArgs) Handles packagecolorLabel.Click
+
+    End Sub
+
+    Private Sub SnackLabel_Click(sender As Object, e As EventArgs) Handles SnackLabel.Click
+
+    End Sub
+
+    Private Sub yummyLabel_Click(sender As Object, e As EventArgs) Handles yummyLabel.Click
+
+    End Sub
+
+    Private Sub packagingLabel_Click(sender As Object, e As EventArgs) Handles packagingLabel.Click
+
+    End Sub
+
+    Private Sub NameLabrel_Click(sender As Object, e As EventArgs) Handles NameLabrel.Click
+
+    End Sub
+
+    Private Sub OreoPictureBox_Click(sender As Object, e As EventArgs) Handles OreoPictureBox.Click
+
+    End Sub
+
+    Private Sub TostitosPictureBox_Click(sender As Object, e As EventArgs) Handles TostitosPictureBox.Click
+
+    End Sub
+End Class
